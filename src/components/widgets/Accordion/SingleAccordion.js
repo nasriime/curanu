60da81ea89469f3513857af3ea53b5735127fbaf
nasriime <mnasr4040@gmail.com>
2019-07-28 21:18:51 +0200

    functional accordion

---
+++ src/components/widgets/Accordion/SingleAccordion.js
@@ -0,0 +1,30 @@
+import React, { useState, useRef } from 'react'
+
+export default function SingleAccordion({title, content}) {
+
+    const [setActive, setActiveState] = useState("");
+    const [setHeight, setHeightState] = useState("0px");
+    const body = useRef(null);
+
+    function toggleAccordion(){
+        setActiveState(setActive === "" ? "active" : "");
+        setHeightState(setActive === "active" ? "0px" : `${body.current.scrollHeight}px`);
+    }
+
+    return (
+        <div className="accordion">
+            <div className={`accordion-header ${setActive}`} onClick={toggleAccordion}>
+                <h2 className="widgetName">{title}</h2>
+                <img className="accordion-icon" src="/images/arrow-right.svg" alt="" />
+            </div>
+            <div className="accordion-body"
+                ref={body}
+                style={{maxHeight: `${setHeight}`}}>
+                      <div
+                        className="accordionText"
+                        dangerouslySetInnerHTML={{ __html: content }}
+                        />
+                </div>
+        </div>
+    )
+}
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeaderContainer from '../components/header/HeaderContainer';
-import Accordion from '../components/widgets/Accordion';
+import AccordionsContainer from '../components/widgets/Accordion/AccordionsContainer';
 // import Evenementen from '../components/widgets/Evenementen';
 // import Nieuws from '../components/widgets/Nieuws';
 // import Add from '../components/widgets/Add';
@@ -18,7 +18,7 @@ export default function home() {
         <div>
             <HeaderContainer/>
             <div className="wrapper">
-                <Accordion/>
+                <AccordionsContainer/>
                 {/* <Evenementen/> */}
                 {/* <Nieuws/> */}
                {/* <Add/> */}
