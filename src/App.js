3ba5549f74cc82a53258c5232a81a850addc754c
nasr_allah <mnasr4040@gmail.com>
2019-07-29 18:49:58 +0200

    react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './assets/style/style.scss';
 import store from './store';
 import Home from './views/Home';
-// import ContactUS from './views/ContactUS';
+import ContactUS from './views/ContactUS';
 
 function App() {
   return (
     <Provider store={store}>
-      <Home />
-      {/* <ContactUS/> */}
+       <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/contact" component={ContactUS} />
+      </Router>
     </Provider>
   );
 }
