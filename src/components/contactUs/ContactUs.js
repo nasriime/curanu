bd0ca224c503c60e459123bb0a506541f2c200ef
nasriime <mnasr4040@gmail.com>
2019-07-29 06:11:43 +0200

    contact form

--- src/components/contactUs/ContactUs.js
+++ src/components/contactUs/ContactUs.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
-export default function ContactUs() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class ContactUs extends Component {
+
+    handleSubmit(values){
+        console.log(values);
+    }
+
+    render() {
+        return (
+            <div className="contact">
+                <h2 className="contact__title">Contact pagina</h2>
+                <p className="contact__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis,
+                magna nec efficitur tempor, dui eros fringilla tellus, vitae consequat augue dolor at mauris. Nunc tempor rutrum dolor,
+                it amet scelerisque lacus feugiat non. Maecenas a tincidunt justo, ac blandit arcu. Sed nec congue ex. 
+                Nullam pulvinar libero eu dolor congue commodo. Nam sed pharetra orci. Nunc ultrices sit amet magna non vestibulum. 
+                Morbi ut dignissim quam. Etiam eget pellentesque nisi.</p>
+                <Formik 
+                    initailValues={{naam:'', email:'', onderwerp:'', bericht:''}}
+                    onSubmit={this.handleSubmit.bind(this)}
+                    validationSchema={Yup.object().shape({
+                        naam: Yup.string().required(),
+                        email: Yup.string().email().required(),
+                        onderwerp: Yup.string().required(),
+                        bericht: Yup.string().required()
+                    })} 
+                    render={ ({
+                        handleChange,
+                        handleSubmit,
+                        isValid,
+                        isSubmitting,
+                        handleBlur,
+                        errors,
+                        touched
+                    }) => (
+                                <form className="contact__form">
+                                    <div>
+                                        <label htmlFor="naam">Naam:</label>
+                                        <input type="text" id="naam" name="naam" 
+                                            className={`${errors.naam && touched.naam ? "invalid" : ""} `}
+                                            onChange={handleChange} 
+                                            onBlur={handleBlur}/>
+                                        {errors.naam && touched.naam ? (<div className="error">{errors.naam}</div>) : null}
+
+                                    </div>
+                                    <div>
+                                        <label htmlFor="email">Email:</label>
+                                        <input type="email" id="email" name="email"
+                                            className={`${errors.email && touched.email ? "invalid" : ""} `}
+                                            onChange={handleChange} 
+                                            onBlur={handleBlur}/>
+                                            {errors.email && touched.email ? (<div className="error">{errors.email}</div>) : null}
+                                    </div>
+                                    <div>
+                                        <label htmlFor="onderwerp">Onderwerp:</label>
+                                        <input type="text" id="onderwerp" name="onderwerp"
+                                            className={`${errors.onderwerp && touched.onderwerp ? "invalid" : ""} `}
+                                            onChange={handleChange} 
+                                            onBlur={handleBlur}/>
+                                            {errors.onderwerp && touched.onderwerp ? (<div className="error">{errors.onderwerp}</div>) : null}
+                                    </div>
+                                    <div>
+                                        <label htmlFor="bericht">Bericht:</label>
+                                        <textarea id="bericht" name="bericht"
+                                            className={`${errors.bericht && touched.bericht ? "invalid" : ""} `}
+                                            onChange={handleChange} 
+                                            onBlur={handleBlur}/>
+                                            {errors.bericht && touched.bericht ? (<div className="error">{errors.bericht}</div>) : null}
+                                    </div>
+                                    <div className="contact__button">
+                                        <button type="button" onClick={handleSubmit} disabled={!isValid || !isSubmitting}>Verstuur</button>
+                                    </div>
+                                </form> 
+                                    )
+                            }/>
+            </div>
+        )
+    }
 }
--- src/components/widgets/Accordion/SingleAccordion.js
+++ src/components/widgets/Accordion/SingleAccordion.js
@@ -4,18 +4,23 @@ export default function SingleAccordion({title, content}) {
 
     const [setActive, setActiveState] = useState("");
     const [setHeight, setHeightState] = useState("0px");
+    const [setRotate, setRotateState] = useState("");
+
     const body = useRef(null);
 
     function toggleAccordion(){
         setActiveState(setActive === "" ? "active" : "");
         setHeightState(setActive === "active" ? "0px" : `${body.current.scrollHeight}px`);
+        setRotateState(
+            setActive === "active" ? "" : "rotate"
+          );
     }
 
     return (
         <div className="accordion">
             <div className={`accordion-header ${setActive}`} onClick={toggleAccordion}>
                 <h2 className="widgetName">{title}</h2>
-                <img className="accordion-icon" src="/images/arrow-right.svg" alt="" />
+                <img className={`accordion-icon ${setRotate}`} src="/images/arrow-right.svg" alt="" />
             </div>
             <div className="accordion-body"
                 ref={body}
