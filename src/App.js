8cfd732f26da039d3ae805799b5b1952feeaf3be
nasriime <mnasr4040@gmail.com>
2019-07-29 07:33:07 +0200

    desktop view

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import './assets/style/style.scss';
 import store from './store';
-// import Home from './views/Home';
-import ContactUS from './views/ContactUS';
+import Home from './views/Home';
+// import ContactUS from './views/ContactUS';
 
 function App() {
   return (
     <Provider store={store}>
-      {/* <Home /> */}
-      <ContactUS/>
+      <Home />
+      {/* <ContactUS/> */}
     </Provider>
   );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,35 +1,43 @@
 import React from 'react'
 import HeaderContainer from '../components/header/HeaderContainer';
 import AccordionsContainer from '../components/widgets/Accordion/AccordionsContainer';
-// import Evenementen from '../components/widgets/Evenementen';
-// import Nieuws from '../components/widgets/Nieuws';
-// import Add from '../components/widgets/Add';
-// import Direct from '../components/widgets/Direct';
-// import Blogs from '../components/widgets/Blogs';
-// import Kwaliteitshandboek from '../components/widgets/Kwaliteitshandboek';
-// import Peilingen from '../components/widgets/Peilingen';
-// import Links from '../components/widgets/Links';
-// import Telefoonboek from '../components/widgets/Telefoonboek';
-// import Groepen from '../components/widgets/Groepen';
-// import Microblog from '../components/widgets/Microblog';
+import Evenementen from '../components/widgets/Evenementen';
+import Nieuws from '../components/widgets/Nieuws';
+import Add from '../components/widgets/Add';
+import Direct from '../components/widgets/Direct';
+import Blogs from '../components/widgets/Blogs';
+import Kwaliteitshandboek from '../components/widgets/Kwaliteitshandboek';
+import Peilingen from '../components/widgets/Peilingen';
+import Links from '../components/widgets/Links';
+import Telefoonboek from '../components/widgets/Telefoonboek';
+import Groepen from '../components/widgets/Groepen';
+import Microblog from '../components/widgets/Microblog';
 
 export default function home() {
     return (
         <div>
             <HeaderContainer/>
             <div className="wrapper">
-                <AccordionsContainer/>
-                {/* <Evenementen/> */}
-                {/* <Nieuws/> */}
-               {/* <Add/> */}
-               {/* <Direct/> */}
-               {/* <Blogs/> */}
-               {/* <Kwaliteitshandboek/> */}
-               {/* <Peilingen/> */}
-               {/* <Links/> */}
-               {/* <Telefoonboek/> */}
-               {/* <Groepen/> */}
-               {/* <Microblog/> */}
+                <div className="display-flex">
+                    <div className="col">
+                        <AccordionsContainer/>
+                        <Evenementen/>
+                        <Nieuws/>
+                        <Add/>
+                    </div>
+                    <div className="col">
+                        <Direct/>
+                        <Blogs/>
+                        <Kwaliteitshandboek/>
+                        <Peilingen/>
+                        <Links/>
+                    </div>
+                    <div className="col">
+                        <Telefoonboek/>
+                        <Groepen/>
+                        <Microblog/>
+                    </div>
+                </div>
             </div>
         </div>
     )
