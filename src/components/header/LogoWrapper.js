b6d5a7970e5e86a28d5c164b6c7ca0033519eb65
nasriime <mnasr4040@gmail.com>
2019-08-11 18:54:28 +0200

    colapse widget

--- src/components/header/LogoWrapper.js
+++ src/components/header/LogoWrapper.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function LogoWrapper() {
-    return (
-        <div className="wrapper">
-            <div className="logoWrapper">
-                <img src="/images/Bitmap.png" alt="logo"/>
-                <div className="logoName">
-                    <h2>Cura<span>Nu</span></h2>  
-                    <h6>Zorg en welzijn</h6>  
-                </div>
-                <div className="intranet">
-                    <h2>Intranet</h2>  
-                </div>
-            </div>
+  return (
+    <div className="wrapper">
+      <div className="logoWrapper">
+        <Link to="/">
+          <img src="/images/Bitmap.png" alt="logo" />
+        </Link>
+        <div className="logoName">
+          <h2>
+            Cura<span>Nu</span>
+          </h2>
+          <h6>Zorg en welzijn</h6>
         </div>
-    )
+        <div className="intranet">
+          <h2>Intranet</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/widgets/Direct.js
+++ src/components/widgets/Direct.js
@@ -1,41 +1,71 @@
-import React from 'react'
+import React from 'react';
 
-export default function Direct() {
+export default class Direct extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: true
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(state => ({
+      active: !state.active
+    }));
+  }
+
+  render() {
+    const { active } = this.state;
+
     return (
-        <div className="widget">
-            <div className="widgetHeader">
-                <div className="header-part-one">
-                    <i><img src="/images/external-link.svg" alt="external-link"/></i>
-                    <h2 className="widgetName">Direct naar</h2>
-                </div>
-                <div className="header-part-two">
-                    <i><img src="/images/arrow-right.svg" alt="arrow-right"/></i>
-                    <i><img src="/images/Shape 2.svg" alt="close"/></i>
-                </div>
-            </div>
-
-            <div className="direct">
-                <div className="direct-item">
-                    <div>
-                        <img src="/images/images.png" alt=""/>
-                    </div>
-                    <p>Youforce</p>
-                </div>
-            
-                <div className="direct-item">
-                    <div>
-                        <img src="/images/Bitmap.png" alt=""/>
-                    </div>
-                    <p>MIP-meldingen</p>
-                </div>
-
-                <div className="direct-item">
-                    <div>
-                        <img src="/images/Bitmap 3.png" alt=""/>
-                    </div>
-                    <p>Topdesk</p>
-                </div>
-            </div>
+      <div className="widget">
+        <div className="widgetHeader" onClick={this.toggle}>
+          <div className="header-part-one">
+            <i>
+              <img src="/images/external-link.svg" alt="external-link" />
+            </i>
+            <h2 className="widgetName">Direct naar</h2>
+          </div>
+          <div className="header-part-two">
+            <i>
+              <img
+                className={active ? 'rotate' : ''}
+                src="/images/arrow-right.svg"
+                alt="arrow-right"
+              />
+            </i>
+            <i>
+              <img src="/images/Shape 2.svg" alt="close" />
+            </i>
+          </div>
         </div>
-    )
+
+        <div className={`widget-body ${active ? 'active' : ''}`}>
+          <div className="direct">
+            <div className="direct-item">
+              <div>
+                <img src="/images/images.png" alt="" />
+              </div>
+              <p>Youforce</p>
+            </div>
+
+            <div className="direct-item">
+              <div>
+                <img src="/images/Bitmap.png" alt="" />
+              </div>
+              <p>MIP-meldingen</p>
+            </div>
+
+            <div className="direct-item">
+              <div>
+                <img src="/images/Bitmap 3.png" alt="" />
+              </div>
+              <p>Topdesk</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
